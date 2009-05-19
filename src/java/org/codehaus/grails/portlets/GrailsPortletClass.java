0667a7f108197d5c198eacd7250386a4850c6e74
k2nakamura <k2nakamura@832c1c66-4827-0410-8465-ccb17913cd09>
2009-05-19 22:11:47 +0000

    Refactored the original plugin and added the support for multiple portal container. Remove the old stuff before checking in the revamped code.
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-portlets/trunk@52211 832c1c66-4827-0410-8465-ccb17913cd09

--- src/java/org/codehaus/grails/portlets/GrailsPortletClass.java
+++
@@ -1,9 +0,0 @@
-package org.codehaus.grails.portlets;
-
-import org.codehaus.groovy.grails.commons.InjectableGrailsClass;
-
-/**
- * @author Lee Butts
- */
-public interface GrailsPortletClass extends InjectableGrailsClass {
-}
