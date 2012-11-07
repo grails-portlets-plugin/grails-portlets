8da7b462568995d06d4920826bd1200df53fc3d3
Burt Beckwith <beckwithb@vmware.com>
2012-11-07 18:37:41 -0500

    moved plugins to BuildConfig.groovy, updated to release version 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,36 +1,35 @@
+grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir	= "target/test-reports"
-grails.project.war.file = "target/${appName}-${appVersion}.war"
-grails.project.dependency.resolution = {
-    inherits "global" // inherit Grails' default dependencies
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-		grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        mavenLocal()
-        mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-    	provided 'javax.portlet:portlet-api:2.0'
-    	runtime('org.springframework:spring-webmvc-portlet:3.0.5.RELEASE') {
-    		excludes 'spring-beans'
-    		excludes 'spring-context'
-    		excludes 'spring-core'
-    		excludes 'spring-web'
-    		excludes 'spring-webmvc'
-    	}
-	// test 'org.jmock:jmock-junit3:2.5.1'
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-    }
 
+grails.project.dependency.resolution = {
+
+	inherits 'global'
+	log 'warn'
+
+	repositories {
+		grailsCentral()
+		mavenLocal()
+		mavenCentral()
+	}
+
+	dependencies {
+
+		provided 'javax.portlet:portlet-api:2.0'
+
+		runtime('org.springframework:spring-webmvc-portlet:3.0.5.RELEASE') {
+			excludes 'spring-beans'
+			excludes 'spring-context'
+			excludes 'spring-core'
+			excludes 'spring-web'
+			excludes 'spring-webmvc'
+		}
+
+		compile 'org.aspectj:aspectjtools:1.6.10'
+	}
+
+	plugins {
+		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
+			export = false
+		}
+	}
 }
